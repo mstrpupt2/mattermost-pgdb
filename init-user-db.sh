f23ef0a05b021c478cf817bcff411b3e428a8dba
mstrpupt2 <seancorkum@yahoo.com>
2017-01-22 11:30:31 -0600

    Update init-user-db.sh

--- init-user-db.sh
+++ init-user-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER docker [ [ WITH ] password [ docker123 ] ];
+    CREATE USER docker WITH PASSWORD 'docker123';
     CREATE DATABASE mattermost;
     GRANT ALL PRIVILEGES ON DATABASE mattermost TO docker;
 EOSQL
