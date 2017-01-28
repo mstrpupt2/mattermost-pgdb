5867e1ed076a5d8b7874133a9fd95cc85efca471
mstrpupt2 <seancorkum@yahoo.com>
2017-01-28 11:34:21 -0600

    Update init-user-db.sh

--- init-user-db.sh
+++ init-user-db.sh
@@ -2,7 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER test WITH PASSWORD 'test';
     CREATE DATABASE mattermost;
-    GRANT ALL PRIVILEGES ON DATABASE mattermost TO docker;
+    GRANT ALL PRIVILEGES ON DATABASE mattermost TO test;
 EOSQL
